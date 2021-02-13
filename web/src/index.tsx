f99303da5e879281026ea923b3567f232e28db57
Simon Boisset <sboisset@ambuliz.com>
2021-02-13 02:00:19 +0100

    bootstrap

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Store from './contexts/Store';
-ReactDOM.render(<Store><App /></Store>, document.getElementById('root'));
+import reportWebVitals from './reportWebVitals';
 
-serviceWorker.unregister();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
