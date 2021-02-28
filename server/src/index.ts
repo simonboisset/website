fe07d683ff5d5e1e1ef546a0b8157d2a81a126a6
Simon Boisset <sboisset@ambuliz.com>
2021-03-01 00:56:55 +0100

    fastify for gcp

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { fastify } from 'fastify';
-import cors from 'fastify-cors';
+// import cors from 'fastify-cors';
 const prisma = new PrismaClient();
 const app = fastify({
   logger: true,
+  trustProxy: true,
 });
-app.register(cors);
+// app.register(cors);
 app.get('/get', async () => {
   const allUsers = await prisma.user.findMany();
   return { allUsers };
@@ -34,3 +35,18 @@ app.listen(8080, function (err) {
     process.exit(1);
   }
 });
+
+async function start() {
+  const IS_GOOGLE_CLOUD_RUN = process.env.K_SERVICE !== undefined;
+  const port = process.env.PORT || 3000;
+  const address = IS_GOOGLE_CLOUD_RUN ? '0.0.0.0' : undefined;
+
+  try {
+    const listen = await app.listen(port, address);
+    console.log(`Listening on ${listen}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+start();
