c763ecd5bdc9a8cf742b4828ac35fbb0605297ee
Simon Boisset <sboisset@ambuliz.com>
2021-03-01 00:21:55 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,10 +28,9 @@ app.get('/create', async () => {
   return { allUsers };
 });
 
-app.listen(8080, function (err, address) {
+app.listen(8080, function (err) {
   if (err) {
     app.log.error({ err });
     process.exit(1);
   }
-  app.log.info(`server listening on ${address}`);
 });
